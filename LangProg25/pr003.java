7e50f062756a0369fecb60bf93c88366cade5fd5
eutopie <animebit818@gmail.com>
2025-11-05 11:08:01 +0700

    Генерация исключений ArrayIndexOutOfBoundsExeption и ArithmeticExeption

---
+++ LangProg25/pr003.java
@@ -0,0 +1,16 @@
+class pr003 {
+	public static void main (String args []) {
+		int nums[] = new int[4];
+		try {
+			System.out.println("До исключительноц ситуации");
+			num [8] = 10;
+			System.out.println ("Строка после исключительной ситуции внури try");
+		}
+		catch (ArrayIndexOfBoundsException) {
+			System.out.println("искл ситуация: выхож за границы массива);
+		System.out.println("После оператора catch")
+		}
+
+	}                                              
+	
+}	
