b9a9ec51ad10ad003e172f38b641f479434f4da8
eutopie <167408345+eutopie@users.noreply.github.com>
2025-11-19 10:25:24 +0700

    Обработка нескольких исключений
    Обработка нескольких исключений и создание исключений вручную

--- LangProg25/pr003.java
+++ LangProg25/pr003.java
@@ -1,16 +1,50 @@
-class pr003 {
-	public static void main (String args []) {
+class ExcTest {
+	//Generating an exception
+	static void genException(){
 		int nums[] = new int[4];
-		try {
-			System.out.println("До исключительноц ситуации");
-			num [8] = 10;
-			System.out.println ("Строка после исключительной ситуции внури try");
+		System.out.println("Before exception");
+		nums[8] = 10;
+		System.out.println("After exception");
+	}
+	static void genException2(){
+		int nums[] = {4, 8, 16, 32, 64, 128, 256};
+		int denominator[] = {2, 4, 4, 0, 4};
+		for (int i = 0; i<nums.length; i++){
+			
+			System.out.println("Before division");
+			System.out.println(nums[i] + "/" + denominator[i] + "="+ nums[i]/denominator[i]);
+			System.out.println("After division");
 		}
-		catch (ArrayIndexOfBoundsException) {
-			System.out.println("искл ситуация: выхож за границы массива);
-		System.out.println("После оператора catch")
-		}
+	}	
+}
 
-	}                                              
-	
-}	
+class pr003 {
+	public static void main (String args[]) {
+		int nums[] = new int[4];
+
+		try{
+			/*
+			System.out.println("Before exception");
+			nums[8] = 10;
+			System.out.println("After exception");
+			*/
+			ExcTest.genException2();
+			ExcTest.genException();
+		}
+		//exc - variable containing a link to Exception class object
+		//ArrayIndex... - is that class for "exc" variable
+		//Throwable is a parent class for all exception classes
+		
+		catch(ArrayIndexOutOfBoundsException exc) {
+			System.out.println("Exception: out of bounds");
+		}
+		catch(Throwable exc) {
+			System.out.println("Exception: " + exc);
+		}
+		//catch(ArithmeticException exc){	
+		//	System.out.println("Exception: invalid arithetic operation");
+		//}
+		System.out.println("After exception catch");
+		
+	}
+}
