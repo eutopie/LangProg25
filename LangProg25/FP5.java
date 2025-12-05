1df12f3b3161afe6b66a5f2223a0ca3338516bb9
eutopie <167408345+eutopie@users.noreply.github.com>
2025-12-05 15:37:04 +0700

    Добавили примеры работы функции replan()

---
+++ LangProg25/FP5.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Kurs {
+    static List<String> replan (List <String> plan, String newChapt, String beforeChapt) {
+        int newChaptIndex = plan.indexOf(beforeChapt);
+        plan.add(newChaptIndex, newChapt);
+        return plan;
+    }
+}
+class FP5 {
+    public static void main (String[] args) {
+        List<String> planA = new ArrayList<>();
+        planA.add("Введение");
+        planA.add("ГЛАВА 1");
+        planA.add("Создание видеоигр");
+        planA.add("ГЛАВА 2");
+        List<String> planB = new ArrayList<>();
+        planB = Kurs.replan(planA, "Анализ целевой аудитории", "ГЛАВА 2");
+        
+    }
+
+}
